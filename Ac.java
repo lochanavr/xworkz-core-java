76d9f4edac770cced8bbf35cb487996a888ea4f8
lochanavr <lochanavr2002@gmail.com>
2024-07-04 10:25:50 +0530

    committing basics related java code

---
+++ Ac.java
@@ -0,0 +1,9 @@
+class Ac{
+   public static void main(String[]panasonic){
+   System.out.println("7 in 1 Convertible Modes with True AI Mode (converti7): True AI mode smartly detects the room's temperature, forecast the cooling capacity and change the fan speed to provide optimal cooling through inbuilt sensors and artificial intelligence. Save energy with adjustment of cooling performance by selecting modes ranging from 40% to 90%, Full capacity and High capacity.");
+    System.out.println("Capacity: 1.5 Ton - Suitable for medium sized rooms (121 sq ft to 170 sq ft). Energy Star Rating: 5 Star | Annual Power Consumption: 774.19 kWh | ISEER: 5.10. The star rating is as per new BEE guidelines. 636 (Indoor) CFM Air Circulation & Ambient Temperature: 16 to 52 degree Celsius.");
+	 System.out.println("Manufacturer Warranty: 1 Year Comprehensive on Product | 5 years on PCB |10 years on Compressor.");
+	  System.out.println("Smart AC: Seamlessly adjust temperature, monitor status and change modes right from the AI enabled Miraie Mobile App. Seamless hands-free operation and Voice Control with Alexa and Hey Google.");
+	   System.out.println("Copper Condenser Coil: Better cooling and requires low maintenance. Healthy Cooling: Equipped with the PM 0.1 Filter to provide dust free, clean air by removing the PM 0.1 particles in the air. 100% Copper Tubing: Helps in better heat exchange and enhances cooling. It also enhances durability of the product via Anti Corrosion Shield Blu Technology");
+   }
+}
